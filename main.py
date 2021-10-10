e35d5cadb1be93be824fd63736b60621ac3f6459
evgeniya35 <evgeniya35@yandex.ru>
2021-10-10 08:11:10 +0300

    Добавил описание `README.md`

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
 import requests
 from requests import exceptions
 from requests.exceptions import HTTPError
 from urllib.parse import urlparse
-from dotenv import load_dotenv
-load_dotenv()
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+TOKEN = os.environ.get("TOKEN")
+
 
 def shorten_link(token, url):
     headers = {
@@ -12,7 +17,10 @@ def shorten_link(token, url):
     options = {
         "long_url": url
     }
-    response = requests.post(url="https://api-ssl.bitly.com/v4/shorten", headers=headers, json=options)
+    response = requests.post(
+        url="https://api-ssl.bitly.com/v4/shorten",
+        headers=headers,
+        json=options)
     response.raise_for_status()
     return response.json()
 
@@ -25,17 +33,12 @@ def count_clicks(token, short_url):
         "unit": "day",
         "units": -1
     }
-    response = requests.get(url=f"https://api-ssl.bitly.com/v4/bitlinks/{short_url}/clicks/summary", headers=headers, params=options)
+    response = requests.get(
+        url=f"https://api-ssl.bitly.com/v4/bitlinks/{short_url}/clicks/summary",
+        headers=headers,
+        params=options)
     response.raise_for_status()
     return response.json()
-
-
-def is_url(url):
-    try:
-        parsed = urlparse(url)
-        return all([parsed.scheme, parsed.netloc])
-    except ValueError:
-        return False
 
 
 def is_bitlink(url):
@@ -46,25 +49,27 @@ def is_bitlink_api(token, parsed):
     headers = {
         "Authorization": "Bearer " + token
     }
-    response = requests.get(url=f"https://api-ssl.bitly.com/v4/bitlinks/{parsed.netloc}{parsed.path}", headers=headers)
+    response = requests.get(
+        url=f"https://api-ssl.bitly.com/v4/bitlinks/{parsed.netloc}{parsed.path}",
+        headers=headers)
     response.raise_for_status()
     bit = response.json()
     return bit["id"] == parsed.netloc + parsed.path
 
-    
+
 def main():
-    url = "https://proglib.io/p/ne-izobretat-velosiped-ili-obzor-modulya-collections-v-python-2019-12-15"
-    token = "cf242e3e8a06d3faadcfcb10650d3726e2a50a57"
-    url = "https://bit.ly/3DpixNc"
+    # url = "https://proglib.io/p/ne-izobretat-velosiped-ili-obzor-modulya-collections-v-python-2019-12-15"
+    # url = "https://bit.ly/3DpixNc"
+    url = input()
     parsed = urlparse(url)
     try:
         if is_bitlink(parsed.netloc):
-            if is_bitlink_api(token, parsed):
-                clicks = count_clicks(token, parsed.netloc + parsed.path)
+            if is_bitlink_api(TOKEN, parsed):
+                clicks = count_clicks(TOKEN, parsed.netloc + parsed.path)
                 print(f"{url} переходов: {clicks['total_clicks']}")
         else:
-             bitlink = shorten_link(token, url)
-             print("Битлинк", bitlink["link"])
+            bitlink = shorten_link(TOKEN, url)
+            print("Битлинк", bitlink["link"])
     except requests.exceptions.HTTPError:
         print("HTTPError in: " + url)
     except Exception as e:
@@ -72,27 +77,6 @@ def main():
     else:
         pass
 
-    
-
-    """
-    try:
-        total_clicks = count_clicks(token, short_url)
-    except requests.exceptions.HTTPError:
-        print(HTTPError)
-    except Exception as e:
-        print(e)
-    else:
-        print("total_clicks", total_clicks["total_clicks"])
-    
-    try:
-        bitlink = shorten_link(token, long_url)["link"]
-    except requests.exceptions.HTTPError:
-        print("HTTPError")
-    except Exception as e:
-        print(e)
-    else:
-        print("Битлинк", bitlink)
-    """
 
 if __name__ == "__main__":
     main()
